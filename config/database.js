c993ba1c462561d54d87fbde6c5d8c2cc9c4172f
Devon Anderson <dvonanderson@gmail.com>
2020-09-15 16:42:30 -0400

    udate database config

--- config/database.js
+++ config/database.js
@@ -5,10 +5,15 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
         useNullAsDefault: true,
+        ssl: false
       },
     },
   },
